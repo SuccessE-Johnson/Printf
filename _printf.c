303a79065817a0ae11640a099b59a7fe6cb2d526
Jummielov <adejumokeadewole@gmail.com>
2023-05-29 03:24:12 -0700

    functions2.c

--- _printf.c
+++ _printf.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * Author - Adejumoke Adewole
- * DATE - 28/05/2023
- * Time - 11:15pm
- * @format: format
+ * _printf - Printf function
+ * @format: format.
  * Return: Printed chars.
  */
-int _printf(const char *format, ...);
+int main()
 {
-	int a, printed = 0, printedChars = 0;
+int _printf(const char *format, ...)
+	int i, printed = 0, pr_c = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -21,29 +19,29 @@ int _printf(const char *format, ...);
 
 	va_start(list, format);
 
-	for (a = 0; format && format[a] != '\0'; a++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[a] != '%')
+		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = format[a];
+			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[a], 1);*/
-			printedChars++;
+			/* write(1, &format[i], 1);*/
+			pr_c++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &a);
-			width = get_width(format, &a, list);
-			precision = get_precision(format, &a, list);
-			size = get_size(format, &a);
-			++a;
-			printed = handle_print(format, &a, list, buffer,
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
-			printedChars += printed;
+			pr_c += printed;
 		}
 	}
 
@@ -51,7 +49,7 @@ int _printf(const char *format, ...);
 
 	va_end(list);
 
-	return (printedChars);
+	return (pr_c);
 }
 
 /**
