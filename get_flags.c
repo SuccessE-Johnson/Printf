8618459fc2ebbd8f11c49b1b127f2c1ab562ca78
SuccessE-Johnson <successdera02@gmail.com>
2023-05-30 10:45:55 -0700

    task update

--- get_flags.c
+++ get_flags.c
@@ -4,11 +4,11 @@
 * DATE - 29/05/2023
 * get_flags - Calculates active flags
 * @format: Formatted string in which to print the arguments
-* @i: take a parameter.
+* @i: take a parameter
 * Return: Flags
 */
 int get_flags(const char *format, int *i);
-{
+({
 /* - + 0 # ' ' */
 /* 1 2 4 8  16 */
 int j, curr_i;
